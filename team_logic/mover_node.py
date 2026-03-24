1ce1f4764bce55255a508268e3d3b6ceae6100c2
Andruha182006 <andryhahurskiy@gmail.com>
2026-03-24 18:27:26 +0000

    Фікс: робот тепер згинається правильно і торкається кубика

--- team_logic/mover_node.py
+++ team_logic/mover_node.py
@@ -3,6 +3,7 @@ import rclpy
 from rclpy.node import Node
 from sensor_msgs.msg import JointState
 import time
+import math
 
 class DobotMover(Node):
     def __init__(self):
@@ -10,34 +11,46 @@ class DobotMover(Node):
         self.joint_pub = self.create_publisher(JointState, '/mg400/joint_states', 10)
         self.timer = self.create_timer(0.02, self.move_loop)
 
-        # Поточні позиції суглобів (з яких починаємо)
         self.current_j = [0.0, 0.0, 0.0]
-        # Коефіцієнт плавності (0.01 - дуже повільно, 0.1 - швидко)
         self.smoothness = 0.05 
-
         self.start_time = time.time()
-        self.get_logger().info('🚀 ПЛАВНИЙ РУХ ЗАПУЩЕНО!')
+        self.get_logger().info('РОБОТ ГОТОВИЙ ДО РУХУ')
 
     def move_loop(self):
         elapsed_time = time.time() - self.start_time
-        cycle_time = elapsed_time % 15.0
+        cycle_time = elapsed_time % 5.0      # 5 сек на кубик
+        total_cycle = elapsed_time % 15.0    # 15 сек на коло
 
-        # Визначаємо ЦІЛЬОВУ позицію (куди хочемо прийти)
-        if cycle_time < 5.0:
-            target_j = [0.0, 0.4, 0.4]    # Червоний
-        elif cycle_time < 10.0:
-            target_j = [1.57, 0.5, 0.5]   # Зелений
+        # 1. ТАРГЕТИ ДЛЯ БАЗИ (Поворот)
+        if total_cycle < 5.0:
+            target_base = 0.0         # Червоний
+        elif total_cycle < 10.0:
+            target_base = 1.57        # Зелений
         else:
-            target_j = [-2.35, 0.3, 0.3]  # Синій
+            target_base = -2.35       # СИНІЙ (Точна координата)
 
-        # МАГІЯ ПЛАВНОСТІ: наближаємо поточне значення до цільового потроху
+        # 2. МАТЕМАТИКА "ЗГИНАННЯ ВГОРІ"
+        # multiplier = 0 (вгорі), multiplier = 1 (торкання кубика)
+        multiplier = 0.5 - 0.5 * math.cos((cycle_time / 5.0) * 2 * math.pi)
+
+        # Плече нахиляється від 0.1 (вертикально) до 0.9 (торкання)
+        bend_factor = 0.1 + (0.9 * multiplier)
+        # Лікоть згинається від 0.0 до 0.5
+        elbow_factor = 0.0 + (0.0 * multiplier)
+
+        target_j = [target_base, bend_factor, elbow_factor]
+
+        # 3. ПЛАВНИЙ РУХ
         for i in range(3):
-            # Формула: поточне = поточне + (ціль - поточне) * швидкість
             self.current_j[i] += (target_j[i] - self.current_j[i]) * self.smoothness
 
+        # 4. ПУБЛІКАЦІЯ СТАНУ
         msg = JointState()
         msg.header.stamp = self.get_clock().now().to_msg()
-        msg.name = ['mg400_j1', 'mg400_j2_1', 'mg400_j2_2', 'mg400_j3_1', 'mg400_j3_2', 'mg400_j4_1', 'mg400_j4_2', 'mg400_j5']
+        msg.name = [
+            'mg400_j1', 'mg400_j2_1', 'mg400_j2_2', 
+            'mg400_j3_1', 'mg400_j3_2', 'mg400_j4_1', 'mg400_j4_2', 'mg400_j5'
+        ]
         
         msg.position = [
             self.current_j[0], 
